06482f28ce9cfee86f099cdce47a99eb4cee6d22
Swingi <swingfire@swingfire.ch>
2026-08-08 13:25:54 +0200

    Delete parts directory

--- parts/filter_slot.py
+++
@@ -1,5 +0,0 @@
-import Part
-
-
-def make_filter_slot(width=3.0, length=150.0, depth=2.0):
-    return Part.makeBox(width, length, depth)
--- parts/guide.py
+++
@@ -1,5 +0,0 @@
-import Part
-
-
-def make_guide(length=150.0, width=3.0, height=10.0):
-    return Part.makeBox(length, width, height)
